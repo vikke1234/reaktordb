f4294f21343053b1693bae309a78f3ed8544bd43
Viktor Horsmanheimo <viktor horsmanheimo>
2021-03-02 21:28:25 +0200

    fixed typo

--- routes/facemasks.js
+++ routes/facemasks.js
@@ -5,11 +5,7 @@ const router = express.Router();
 
 router.get("/facemasks", async (req, res) => {
   const data = await getProducts("facemasks");
-  res.status(200).end(data);
-});
-
-router.get("/facemasks/:id", (req, res) => {
-  getProducts("facemasks").map((mask) => mask == req.params.id);
+  res.status(200).send(data);
 });
 
 module.exports = router;
